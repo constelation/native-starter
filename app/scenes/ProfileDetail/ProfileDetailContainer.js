5fbb338782aba19ce22c2386afa6f401b7a1a561
Kyle Poole <2kylepoole@gmail.com>
2017-01-05 12:51:00 -0800

    update libs

--- app/scenes/ProfileDetail/ProfileDetailContainer.js
+++ app/scenes/ProfileDetail/ProfileDetailContainer.js
@@ -9,23 +9,23 @@ import ProfileDetail from './ProfileDetail'
 
 // }}}
 
-export default inject('counter')(observer(
-  class ProfileDetailContainer extends React.Component {
-    props: {
-      counter: Counter
-    }
-
-    handleIncreaseCounter = () => {
-      this.props.counter.increase()
-    }
-
-    render() {
-      return (
-        <ProfileDetail
-          value={this.props.counter.value}
-          onIncreaseCounter={this.handleIncreaseCounter}
-        />
-      )
-    }
+class ProfileDetailContainer extends React.Component {
+  props: {
+    counter: Counter
   }
-))
+
+  handleIncreaseCounter = () => {
+    this.props.counter.increase()
+  }
+
+  render() {
+    return (
+      <ProfileDetail
+        value={this.props.counter.value}
+        onIncreaseCounter={this.handleIncreaseCounter}
+      />
+    )
+  }
+}
+
+export default inject('counter')(observer(ProfileDetailContainer))
